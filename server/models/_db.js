38256cb48a392086d203a07a64a6be7fed7c9f06
bmmanson <bmmanson@gmail.com>
2016-10-27 19:39:24 +0300

    adjusted db for heroku

--- server/models/_db.js
+++ server/models/_db.js
@@ -2,8 +2,27 @@ var Sequelize = require('sequelize');
 
 var env = require('./../env');
 
-var db = new Sequelize(env.DATABASE_URI, {
-  logging: false
-});
+var db;
+
+if (env.NODE_ENV === 'production') {
+    // the application is executed on Heroku ... use the postgres database
+    db = new Sequelize(env.DATABASE_URI, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true 
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    db = new Sequelize(env.DATABASE_URI, {
+	  logging: false
+	});
+  }
+
+// original: 
+// var db = new Sequelize(env.DATABASE_URI, {
+//   logging: false
+// });
 
 module.exports = db;
